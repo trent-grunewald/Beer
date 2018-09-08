186ee519cb313ec6d138938c7fa5b71875e06c09
trent-grunewald <39209726+trent-grunewald@users.noreply.github.com>
2018-09-07 20:58:19 -0700

    updates

---
+++ carousel.js
@@ -0,0 +1,4 @@
+const carousel = document.querySelector('.grid-carousel');
+const slides = carousel.querySelectorAll('.grid-carousel__item');
+const leftButton = carousel.querySelector('.js-left');
+const rightButton = carousel.querySelector('.js-right');
