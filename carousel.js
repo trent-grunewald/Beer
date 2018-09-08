1c638b079c3309d72bf52488323357f82fba5c29
trent-grunewald <39209726+trent-grunewald@users.noreply.github.com>
2018-09-07 23:26:13 -0700

    carousel updates

--- carousel.js
+++ carousel.js
@@ -3,10 +3,7 @@ const slides = carousel.querySelectorAll('.grid-carousel__item');
 const leftButton = carousel.querySelector('.js-left');
 const rightButton = carousel.querySelector('.js-right');
 
-//Event Listeners
-rightButton.addEventListener('click', _ => {
-  moveRight();
-})
+
 
 const getOrder = (elem) => {
   const styles = getComputedStyle(elem);
@@ -16,15 +13,48 @@ const getOrder = (elem) => {
   return order;
 }
 
+const featuredItem = e => {
+  if (order == 3) {
+    e.classList.add('featured');
+  } else {
+    e.classList.remove('featured');
+  }
+}
+
 const moveRight =_=> {
   slides.forEach(function(slide) {
     order = getOrder(slide);
 
+    if (order > 1) {
+      slide.style.order = order -= 1;
+    } else {
+      slide.style.order = 5;
+    }
+
+    featuredItem(slide);
+  });
+}
+
+const moveLeft =_=> {
+  slides.forEach(function(slide) {
+    order = getOrder(slide);
+    
     if (order < slides.length) {
       slide.style.order = order += 1;
     } else {
       slide.style.order = 1;
     }
+    
+    featuredItem(slide);
+
   });
 }
 
+//Event Listeners
+rightButton.addEventListener('click', _ => {
+  moveRight();
+})
+
+leftButton.addEventListener('click', _ => {
+  moveLeft();
+})
