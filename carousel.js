cf056eb522f209115cec71c37a1efba90aefb1fb
trent-grunewald <39209726+trent-grunewald@users.noreply.github.com>
2018-09-07 22:00:08 -0700

    Right button carousel
    functionality

--- carousel.js
+++ carousel.js
@@ -2,3 +2,29 @@ const carousel = document.querySelector('.grid-carousel');
 const slides = carousel.querySelectorAll('.grid-carousel__item');
 const leftButton = carousel.querySelector('.js-left');
 const rightButton = carousel.querySelector('.js-right');
+
+//Event Listeners
+rightButton.addEventListener('click', _ => {
+  moveRight();
+})
+
+const getOrder = (elem) => {
+  const styles = getComputedStyle(elem);
+  const orderValue = styles.order;
+  //turns it into a number instead of string
+  const order = parseInt(orderValue);
+  return order;
+}
+
+const moveRight =_=> {
+  slides.forEach(function(slide) {
+    order = getOrder(slide);
+
+    if (order < slides.length) {
+      slide.style.order = order += 1;
+    } else {
+      slide.style.order = 1;
+    }
+  });
+}
+
