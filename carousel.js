aba690c070e6eeca3cb7e4feed4bebcecb3ac13a
trent-grunewald <39209726+trent-grunewald@users.noreply.github.com>
2018-09-10 18:54:16 -0700

    Carousel

--- carousel.js
+++ carousel.js
@@ -52,9 +52,9 @@ const moveLeft =_=> {
 
 //Event Listeners
 rightButton.addEventListener('click', _ => {
-  moveRight();
+  moveLeft();
 })
 
 leftButton.addEventListener('click', _ => {
-  moveLeft();
+  moveRight();
 })
